f407bd8d1cd40ba542a4272be651579c0bc84d16
오유찬 <oyuchan50@gmail.com>
2024-11-18 09:30:42 +0900

    :feat: initial commit

---
+++ src/main/java/com/mycom/backenddaengplace/BackEndDaengPlaceApplication.java
@@ -0,0 +1,13 @@
+package com.mycom.backenddaengplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndDaengPlaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndDaengPlaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mycom/backenddaengplace/BackEndDaengPlaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycom.backenddaengplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndDaengPlaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
