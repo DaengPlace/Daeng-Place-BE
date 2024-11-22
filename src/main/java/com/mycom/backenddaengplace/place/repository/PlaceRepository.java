d304552935adc60056a11055d6f4377da004302e
jaemin <sai06266@naver.com>
2024-11-22 14:41:55 +0900

    :sparkles: 장소 상세 조회 구현

---
+++ src/main/java/com/mycom/backenddaengplace/place/repository/PlaceRepository.java
@@ -0,0 +1,9 @@
+package com.mycom.backenddaengplace.place.repository;
+
+
+import com.mycom.backenddaengplace.place.domain.Place;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlaceRepository extends JpaRepository<Place, Long> {
+
+}
