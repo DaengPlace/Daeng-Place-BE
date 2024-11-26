318327a06a046de45eac5c14af73bf168cf1c536
오유찬 <oyuchan50@gmail.com>
2024-11-26 11:06:09 +0900

    :sparkles: 예외 처리 분리

--- src/main/java/com/mycom/backenddaengplace/pet/exception/BreedNotFoundException.java
+++ src/main/java/com/mycom/backenddaengplace/pet/exception/BreedNotFoundException.java
@@ -1,8 +1,9 @@
 package com.mycom.backenddaengplace.pet.exception;
 
+import com.mycom.backenddaengplace.common.exception.BaseException;
 import org.springframework.http.HttpStatus;
 
-public class BreedNotFoundException extends PetException{
+public class BreedNotFoundException extends BaseException {
     public BreedNotFoundException(Long breedTypeId) {
         super(String.format("견종 ID [%d]를 찾을 수 없습니다.", breedTypeId),
                 HttpStatus.NOT_FOUND);
