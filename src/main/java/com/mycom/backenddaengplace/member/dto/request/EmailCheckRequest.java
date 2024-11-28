036c485d5cff234bacd238950af4c1a9c8a707d8
pardessuccess <zkwhdgur238@gmail.com>
2024-11-28 15:32:36 +0900

    :feat: Member 수정 및 삭제 설정

---
+++ src/main/java/com/mycom/backenddaengplace/member/dto/request/EmailCheckRequest.java
@@ -0,0 +1,17 @@
+package com.mycom.backenddaengplace.member.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class EmailCheckRequest {
+
+    @NotEmpty(message = "이메일은 입력해주세요.")
+    @Email(message = "이메일 형식에 맞게 입력해주세요.")
+    private String email;
+
+}
