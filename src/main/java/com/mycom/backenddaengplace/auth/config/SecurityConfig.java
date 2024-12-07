4f7df608dce1802c43501f72cd8676ab078785fd
Ohyuchan <oyuchan50@gmail.com>
2024-12-08 02:00:55 +0900

    Update SecurityConfig.java

--- src/main/java/com/mycom/backenddaengplace/auth/config/SecurityConfig.java
+++ src/main/java/com/mycom/backenddaengplace/auth/config/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
 
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/login", "/oauth2/**", "/auth/**", "/reissue"
-                                ,"/health", "/").permitAll()
+                                ,"/health", "/hc").permitAll()
                         .anyRequest().authenticated())
 
                 .oauth2Login(oauth2 -> oauth2
