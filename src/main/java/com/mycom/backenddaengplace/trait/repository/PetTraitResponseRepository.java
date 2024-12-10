9ad0f29a0508012e3825a850cab588ac39c3d8cd
jaemin <sai06266@naver.com>
2024-12-10 19:39:40 +0900

    :sparkles: 반려견 성향 조회 기능 구현

--- src/main/java/com/mycom/backenddaengplace/trait/repository/PetTraitResponseRepository.java
+++ src/main/java/com/mycom/backenddaengplace/trait/repository/PetTraitResponseRepository.java
@@ -4,5 +4,8 @@ import com.mycom.backenddaengplace.trait.domain.PetTraitResponse;
 import com.mycom.backenddaengplace.trait.domain.PetTraitResponseId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PetTraitResponseRepository extends JpaRepository<PetTraitResponse, PetTraitResponseId> {
+    List<PetTraitResponse> findByPetId(Long petId);
 }
