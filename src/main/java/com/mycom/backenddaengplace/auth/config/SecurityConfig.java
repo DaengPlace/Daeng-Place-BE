7aeaece1856f0d95b8faf83c542a14d089ef1e9c
오유찬 <oyuchan50@gmail.com>
2024-12-10 15:49:14 +0900

    :recycle: csrf 설정 cors 설정 위치 변경

--- src/main/java/com/mycom/backenddaengplace/auth/config/SecurityConfig.java
+++ src/main/java/com/mycom/backenddaengplace/auth/config/SecurityConfig.java
@@ -29,8 +29,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable)
                 .cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .formLogin(AbstractHttpConfigurer::disable)
