a3b681524ece948e2282136dd075ed3020694c12
오유찬 <oyuchan50@gmail.com>
2024-12-14 03:28:41 +0900

    :fire: 불필요한 파일 제거

--- src/main/java/com/mycom/backenddaengplace/member/controller/MemberController.java
+++ src/main/java/com/mycom/backenddaengplace/member/controller/MemberController.java
@@ -4,25 +4,17 @@ package com.mycom.backenddaengplace.member.controller;
 import com.mycom.backenddaengplace.auth.dto.CustomOAuth2User;
 import com.mycom.backenddaengplace.common.dto.ApiResponse;
 import com.mycom.backenddaengplace.member.domain.Member;
-import com.mycom.backenddaengplace.member.dto.request.BaseEmailRequest;
-import com.mycom.backenddaengplace.member.dto.request.EmailCodeCheckRequest;
 import com.mycom.backenddaengplace.member.dto.request.MemberRegisterRequest;
 import com.mycom.backenddaengplace.member.dto.request.MemberUpdateRequest;
-import com.mycom.backenddaengplace.member.dto.response.EmailCodeCheckResponse;
-import com.mycom.backenddaengplace.member.dto.response.EmailDuplicateCheckResponse;
 import com.mycom.backenddaengplace.member.dto.response.BaseMemberResponse;
-import com.mycom.backenddaengplace.member.dto.response.EmailSendCodeResponse;
 import com.mycom.backenddaengplace.member.service.EmailService;
 import com.mycom.backenddaengplace.member.service.MemberService;
-import jakarta.mail.MessagingException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-
-import java.io.UnsupportedEncodingException;
 
 @RestController
 @RequestMapping("/members")
