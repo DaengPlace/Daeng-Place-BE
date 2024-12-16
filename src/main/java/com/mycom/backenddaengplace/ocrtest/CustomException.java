9e2dd044e0c8b66dcf347c36f2eb29e7cb9f9408
jaemin <sai06266@naver.com>
2024-12-16 15:00:42 +0900

    :fire: 불필요한 파일 제거(OCR), 패키지명 수정

--- src/main/java/com/mycom/backenddaengplace/ocrtest/CustomException.java
+++
@@ -1,7 +0,0 @@
-package com.mycom.backenddaengplace.ocrtest;
-
-public class CustomException extends RuntimeException {
-    public CustomException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/mycom/backenddaengplace/ocrtest/repository/OcrResultRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mycom.backenddaengplace.ocrtest.repository;
-
-import com.mycom.backenddaengplace.ocrtest.domain.OcrResult;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OcrResultRepository extends JpaRepository<OcrResult, Long> {
-}
