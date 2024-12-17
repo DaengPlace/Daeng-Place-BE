1def4b2bb80d361241a7fbcd595db043db0af19b
pardessuccess <zkwhdgur238@gmail.com>
2024-12-18 01:56:17 +0900

    :recycle: Application EnableScheduling 추가

--- src/main/java/com/mycom/backenddaengplace/BackEndDaengPlaceApplication.java
+++ src/main/java/com/mycom/backenddaengplace/BackEndDaengPlaceApplication.java
@@ -2,12 +2,12 @@ package com.mycom.backenddaengplace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class BackEndDaengPlaceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BackEndDaengPlaceApplication.class, args);
     }
-
 }
