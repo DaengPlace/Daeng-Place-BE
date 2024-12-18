6f73c0c0127519c6eb8eee27ce8e9edd6035242f
pardessuccess <zkwhdgur238@gmail.com>
2024-12-18 16:56:33 +0900

    :recycle: TraitTagCountRepository 오류 쿼리 추가

--- src/main/java/com/mycom/backenddaengplace/trait/repository/TraitTagCountRepository.java
+++ src/main/java/com/mycom/backenddaengplace/trait/repository/TraitTagCountRepository.java
@@ -3,7 +3,11 @@ package com.mycom.backenddaengplace.trait.repository;
 import com.mycom.backenddaengplace.trait.domain.TraitTagCount;
 import com.mycom.backenddaengplace.trait.domain.TraitTagCountId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface TraitTagCountRepository extends JpaRepository<TraitTagCount, TraitTagCountId> {
+    @Modifying
+    @Query("DELETE FROM TraitTagCount ttc WHERE ttc.review.id = :reviewId")
     void deleteByReviewId(Long reviewId);
 }
